77bfa4ba40375bce29382a7c69bcefc602e2c299
hamoo22 <44175127+hamoo22@users.noreply.github.com>
2018-10-18 18:46:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('ready', () => {
     client.user.setStatus("idle")
 });
 //by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-const prefix = "M"
+const prefix = "A"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
